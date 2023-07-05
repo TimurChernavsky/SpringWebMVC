376933d7c2c070973ea89d540984e4c7b4ba0ec9
Тимур <120785772+TimurChernavsky@users.noreply.github.com>
2023-07-05 09:09:42 +0300

    Update PostController.java

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -1,21 +1,13 @@
 package ru.netology.controller;
-
-import com.google.gson.Gson;
-import jakarta.servlet.http.HttpServletResponse;
-import org.example.model.Post;
-import org.example.service.PostService;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-import java.io.Reader;
 import java.util.List;
+import ru.netology.model.Post;
+import ru.netology.service.PostService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
-    public static final String APPLICATION_JSON = "application/json";
     private final PostService service;
-    private final Gson gson = new Gson();
 
     public PostController(PostService service) {
         this.service = service;
@@ -27,24 +19,17 @@ public class PostController {
     }
 
     @GetMapping("/{id}")
-    public void getById(@PathVariable long id, HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        Post post = service.getById(id);
-        response.getWriter().print(gson.toJson(post));
+    public Post getById(@PathVariable long id) {
+        return service.getById(id);
     }
 
     @PostMapping
-    public void save(Reader body, HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        final var post = gson.fromJson(body, Post.class);
-        final var data = service.save(post);
-        response.getWriter().print(gson.toJson(data));
+    public Post save(@RequestBody Post post) {
+        return service.save(post);
     }
 
     @DeleteMapping("/{id}")
-    public void removeById(@PathVariable long id, HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
+    public void removeById(long id) {
         service.removeById(id);
-        response.getWriter().print("Post with id=" + id + " deleted");
     }
 }
